927e9f2f91282322e2a97b4796a9c0c312dca8a6
Huyen Vu <huyenvungoc17@gmail.com>
2023-07-18 16:49:36 +0700

    add database indexes, install formidable package

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ const port = 4000
 
 app.use(express.json())
 app.use('/users', usersRouter)
-databaseService.connect()
+databaseService.connect().then(() => {
+  databaseService.indexUsers()
+  databaseService.indexRefreshTokens()
+  databaseService.indexFollowers()
+})
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
